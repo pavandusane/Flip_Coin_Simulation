7c3a941e97f725433daef4920fd2489ba25e1a5e
pavandusane <dusanepv7@gmail.com>
2020-09-30 00:34:33 +0530

    Added : add min 2 if tie

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -42,6 +42,24 @@ then
 elif [ $count1 -eq $count ]
 then
 	echo "It is tie"
+  while [ true ]
+     do
+        num1=$(( $RANDOM % 2 ))
+	echo $num1
+	if [ $num1 -eq 0 ]
+   	then
+        echo "Head"
+        ((count++))
+	else
+        echo "Tail"
+        ((count1++))
+	fi
+	diff=$(( $count1 - $count ))
+	if [ $diff -gt 2 ] || [ $diff -gt -2 ]
+	then break
+	else
+	continue
+        fi
+     done
+
 fi
-
-
