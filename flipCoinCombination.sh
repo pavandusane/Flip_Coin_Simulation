0c6b3f0a35cfe508332a6d8177fe22a7f79fc49b
pavandusane <dusanepv7@gmail.com>
2020-09-29 23:26:23 +0530

    Added : display percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+declare -A dict1
+
+count1=0
+count2=0
 
 echo "welcome to flip coin simulation problem"
 
+
+for (( i=1;i<=20;i++ ))
+do
 num=$(( $RANDOM % 2 ))
-if [ $num -eq 0 ]
-   then
+  if [ $num -eq 0 ]
+    then
         echo "Head"
-else
+	##count1=(($count1+1))
+	let count1++
+  else
         echo "Tail"
-fi
+	##count2=(($count2+1))
+	let count2++
+  fi
+done
+
+echo $count1
+echo $count2
+
+dict1[Head]="$count1"
+dict1[Tail]="$count2"
+
+percentage_of_head=$(( $count1 * 5 ))
+percentage_of_tail=$(( $count2 * 5 ))
+
+echo "Percentage_of_head : $percentage_of_head"
+echo "Percentage_of_tail : $percentage_of_tail"
